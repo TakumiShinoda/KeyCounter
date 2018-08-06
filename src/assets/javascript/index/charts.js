334ab1a4fbe27f92d9de878350c7ab429fc001fb
TakimiShinoda <godgodx4@gmail.com>
2018-08-06 14:00:13 +0900

    add new graph sample

--- src/assets/javascript/index/charts.js
+++ src/assets/javascript/index/charts.js
@@ -31,9 +31,68 @@ module.exports = {
       },
     },
     xAxis: {
+      show: true,
       data: ["Sun.", "Mon.", "Tue.", "Wed.", "Thurs.", "Fri.", "Sat."],
     },
-    yAxis: {},
+    yAxis: {
+      show: true,
+    },
+    series: [
+      {
+        name: 'Ave',
+        type: 'line',
+        data: [5, 20, 36, 10, 10, 20, 10],
+      },
+      {
+        name: 'This',
+        type: 'line',
+        data: [10, 10, 72, 5, 20, 10, 5],
+      },
+      {
+        name: 'Last',
+        type: 'line',
+        data: [30, 2, 50, 20, 10, 25, 25],
+      },
+    ],
+  },
+  daily: {
+    color : [
+      'rgba(20,255,212,1)',
+      'rgba(20, 155, 212, 1)',
+      'rgba(255, 20, 181, 1)'
+    ],
+    textStyle: {
+      color: 'rgba(255, 255, 255, 1)',
+    },
+    backgroundColor: 'rgba(34, 37, 43, 0)',
+    title: {
+      text: 'Daily Types',
+      textStyle: {
+        color: 'rgba(255, 255, 255, 1)',
+      },
+      x: 'center',
+    },
+    tooltip: {},
+    legend: {
+      x: '90%',
+      y: '25%',
+      orient: 'vertical',
+      data:[
+        'Ave',
+        'This',
+        'Last'
+      ],
+      textStyle: {
+        color: 'rgba(255, 255, 255, 1)',
+      },
+    },
+    xAxis: {
+      show: true,
+      data: ["Sun.", "Mon.", "Tue.", "Wed.", "Thurs.", "Fri.", "Sat."],
+    },
+    yAxis: {
+      show: true,
+    },
     series: [
       {
         name: 'Ave',
--- src/assets/javascript/index/index.js
+++ src/assets/javascript/index/index.js
@@ -3,7 +3,60 @@ const Com = require('../common');
 const Contents = require('./contentsController.js');
 
 $(document).ready(() => {
-  var myChart = echarts.init(document.getElementById('test'));
+  let WeeklyGraph = echarts.init(document.getElementById('weekly'));
+  let DailyGraph = echarts.init(document.getElementById('daily'));
+
+  let OverviewGraphs = {
+    weekly: {
+      graph: WeeklyGraph,
+      option: ChartOptions.weekly,
+    },
+    daily: {
+      graph: DailyGraph,
+      option: ChartOptions.daily,
+    }
+  }
+
+  $('.overviewGraph').click((e) => {
+    let graphName = e.currentTarget.id;
+    let targetGraph = OverviewGraphs[graphName];
+    let keys = Object.keys(OverviewGraphs);
+
+    for(var i = 0; i < keys.length; i++){
+      if(keys[i] != graphName){
+        $('#' + keys[i]).animate({
+          opacity: 0,
+        }, 500);
+      }
+    }
+
+    $('#' + graphName).removeClass('col-6');
+    $('#' + graphName).removeClass('col-9');
+
+    setTimeout(() => {
+      targetGraph.option.xAxis.show = false;
+      targetGraph.option.yAxis.show = false;
+
+      $('#' + graphName).animate({
+        width: "600px",
+        height: "320px"
+      },
+      {
+        duration: "slow",
+        easing: "swing",
+        step: () => {
+          targetGraph.graph.resize();
+          targetGraph.graph.setOption(targetGraph.option);
+        },
+        complete: () => {
+          targetGraph.option.xAxis.show = true;
+          targetGraph.option.yAxis.show = true;
+          targetGraph.graph.resize();
+          targetGraph.graph.setOption(targetGraph.option);
+        },
+      });
+    }, 1000);
+  });
 
   $('#menues li').click((e) => {
     var name = e.target.outerText.trim();
@@ -30,5 +83,6 @@ $(document).ready(() => {
     }
   });
 
-  myChart.setOption(ChartOptions.weekly);
+  WeeklyGraph.setOption(ChartOptions.weekly);
+  DailyGraph.setOption(ChartOptions.daily);
 });
