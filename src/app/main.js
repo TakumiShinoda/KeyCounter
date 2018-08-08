376230ff085de2d8d978a4121effe564839c7a92
TakimiShinoda <godgodx4@gmail.com>
2018-08-08 18:02:36 +0900

    add electron-json-storage for save data

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,7 @@ const {app, BrowserWindow, dialog} = require('electron');
 const {distPath} = require('../../dev/path');
 const ipc = require('electron').ipcMain;
 const KeyObserber = require('./module/KeyObserber');
+const Storage = require('electron-json-storage');
 
 const CheckPermissionOption = {
   width: 500,
@@ -30,7 +31,30 @@ app.on('ready', () => {
       .then(() => {
         console.log("suc");
         KeyObserber.keyListener((data) => {
-          console.log(data.toString());
+          let post = data.toString();
+          let params = post.split(' ');
+          let now = new Date(parseInt(params[0]) * 1000).toLocaleString();
+          let today = now.split(' ')[0].split('/').join('-');
+          let postData = {epoch: params[0], key: params[1]};
+
+          // console.log(params);
+
+          Storage.get(today, (err, jsonObj) => {
+            if(err) console.log(err);
+
+            console.log(jsonObj);
+            if(Object.keys(jsonObj) == 0){
+              Storage.set(today, {log:[]}, (err) => {
+                if(err) console.log(err);
+              })
+            }else{
+              jsonObj.log.push(postData);
+              Storage.set(today, jsonObj, (err) => {
+                if(err) console.log(err);
+              });
+            }
+          })
+
         }, (err) => {
           console.log(err.toString());
           dialog.showErrorBox('Error', 'Cause launch error.');
